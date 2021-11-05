314c8397e0a2e253e96a3f1b4c79627d134f8728
Thomas F. K. Jorna <hello@tefkah.com>
2021-11-05 13:58:41 +0100

    feat: list commits (sorta)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,4 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
+import { Link as ChakraLink, Text, Code, List, ListIcon, ListItem, Flex } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 
 import { Hero } from '../components/Hero'
@@ -14,43 +7,75 @@ import { Main } from '../components/Main'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
+import React, { useEffect, useState } from 'react'
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
-      </Text>
+const getCommits = async () => {
+  const dt = await fetch('https://api.github.com/repos/ThomasFKJorna/thesis-writings/commits', {
+    method: 'GET',
+    headers: { Accept: 'application/vnd.github.v3+json' },
+  })
+  const dtjs = await dt.json()
+  return dtjs
+}
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+const getCommitInfo = async (data) => {
+  const commits = data.map(async (commit) => {
+    try {
+      return await fetch(
+        `https://api.github.com/repos/org-roam/org-roam-ui/git/commits/${commit}`,
+        {
+          method: 'GET',
+          headers: { Accept: 'application/vnd.github.v3+json' },
+        },
+      ).then((res) => res.json())
+    } catch (e) {
+      return e
+    }
+  })
+  await Promise.all(commits)
+  return commits
+}
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+const getBigData = async () => {
+  const commits = await getCommits()
+  const specificCommits = await getCommitInfo(commits)
+
+  return { commits, specificCommits }
+}
+
+const Index = () => {
+  const [data, setData] = useState<any>()
+  useEffect(() => {
+    if (!data) {
+      getBigData().then((res) => setData(res))
+    }
+    return () => {}
+  }, [])
+
+  //  const enumdata =
+  return (
+    <Container height="100vh">
+      <Hero title="Somehow a philosophy thesis" />
+      <Main>
+        <Text>Testing fetching</Text>
+      </Main>
+
+      {data?.commits?.map((commit) => {
+        return (
+          <Flex justifyContent="space-between" key={commit.sha} width="80% ">
+            <Text>{commit.commit.message}</Text>
+            <Text>{commit.commit.author.date}</Text>
+            <Text>{commit.sha}</Text>
+          </Flex>
+        )
+      })}
+      <DarkModeSwitch />
+      <Footer>
+        <Text>Next ❤️ Chakra</Text>
+      </Footer>
+      <CTA />
+    </Container>
+  )
+}
 
 export default Index
