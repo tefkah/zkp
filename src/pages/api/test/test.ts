0ec62ce68b0cd3d789b5f6be2438883c91998876
Thomas F. K. Jorna <hello@tefkah.com>
2021-11-10 16:17:42 +0100

    feat(diff): don't refer to the public folder with /public, that doesn't work

--- src/pages/api/test/test.ts
+++ src/pages/api/test/test.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const x = fs.readFileSync('public/notes/so_2.org', { encoding: 'utf8' })
+  const x = fs.readFileSync('notes/so_2.org', { encoding: 'utf8' })
 
   res.status(200).json([x])
 }
