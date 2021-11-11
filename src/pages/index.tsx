b9e3de7a61559d8c02dbe278c8627b5d131f739e
Thomas F. K. Jorna <hello@tefkah.com>
2021-11-11 11:28:28 +0100

    chore(diff): cleanin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,6 @@ import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
 import React, { useEffect, useMemo, useState } from 'react'
 import { fetchDiff } from '../server/gitlab'
 import useSWR from 'swr'
@@ -31,96 +29,6 @@ import { TestOrg } from '../components/testOrg'
 import { Commit, getListOfCommitsWithStats } from '../utils/getListOfCommitsWithStats'
 import { join } from 'path'
 
-const babyData = [
-  {
-    id: 'd655814be4a0c8ad5128fc6dd0c26b82be8d49f1',
-    short_id: 'd655814b',
-    created_at: '2021-11-04T15:14:35.000+01:00',
-    parent_ids: ['3b01166ffed669d6c900cf5e4176a54132f05cf0'],
-    title: 'Create README.org',
-    message: 'Create README.org',
-    author_name: 'Thomas F. K. Jorna',
-    author_email: 'hello@tefkah.com',
-    authored_date: '2021-11-04T15:14:35.000+01:00',
-    committer_name: 'GitHub',
-    committer_email: 'noreply@github.com',
-    committed_date: '2021-11-04T15:14:35.000+01:00',
-    trailers: {},
-    web_url:
-      'https://gitlab.com/ThomasFKJorna/thesis-writing/-/commit/d655814be4a0c8ad5128fc6dd0c26b82be8d49f1',
-    stats: {
-      additions: 15,
-      deletions: 0,
-      total: 15,
-    },
-  },
-  {
-    id: '3b01166ffed669d6c900cf5e4176a54132f05cf0',
-    short_id: '3b01166f',
-    created_at: '2021-11-04T14:55:13.000+01:00',
-    parent_ids: ['979651681530eb4d5381d4d6ff46b406c839a105'],
-    title: 'meta: add CC license',
-    message: 'meta: add CC license',
-    author_name: 'Thomas F. K. Jorna',
-    author_email: 'hello@tefkah.com',
-    authored_date: '2021-11-04T14:55:13.000+01:00',
-    committer_name: 'GitHub',
-    committer_email: 'noreply@github.com',
-    committed_date: '2021-11-04T14:55:13.000+01:00',
-    trailers: {},
-    web_url:
-      'https://gitlab.com/ThomasFKJorna/thesis-writing/-/commit/3b01166ffed669d6c900cf5e4176a54132f05cf0',
-    stats: {
-      additions: 173,
-      deletions: 0,
-      total: 173,
-    },
-  },
-  {
-    id: '979651681530eb4d5381d4d6ff46b406c839a105',
-    short_id: '97965168',
-    created_at: '2021-11-04T14:51:34.000+01:00',
-    parent_ids: [],
-    title: 'Initial commit',
-    message: 'Initial commit',
-    author_name: 'Thomas F. K. Jorna',
-    author_email: 'hello@tefkah.com',
-    authored_date: '2021-11-04T14:51:34.000+01:00',
-    committer_name: 'GitHub',
-    committer_email: 'noreply@github.com',
-    committed_date: '2021-11-04T14:51:34.000+01:00',
-    trailers: {},
-    web_url:
-      'https://gitlab.com/ThomasFKJorna/thesis-writing/-/commit/979651681530eb4d5381d4d6ff46b406c839a105',
-    stats: {
-      additions: 276,
-      deletions: 0,
-      total: 276,
-    },
-  },
-]
-const singleComm = {
-  id: '979651681530eb4d5381d4d6ff46b406c839a105',
-  short_id: '97965168',
-  created_at: '2021-11-04T14:51:34.000+01:00',
-  parent_ids: [],
-  title: 'Initial commit',
-  message: 'Initial commit',
-  author_name: 'Thomas F. K. Jorna',
-  author_email: 'hello@tefkah.com',
-  authored_date: '2021-11-04T14:51:34.000+01:00',
-  committer_name: 'GitHub',
-  committer_email: 'noreply@github.com',
-  committed_date: '2021-11-04T14:51:34.000+01:00',
-  trailers: {},
-  web_url:
-    'https://gitlab.com/ThomasFKJorna/thesis-writing/-/commit/979651681530eb4d5381d4d6ff46b406c839a105',
-  stats: {
-    additions: 276,
-    deletions: 0,
-    total: 276,
-  },
-}
 export interface Committ {
   id: string
   short_id: string
