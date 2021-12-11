f511dd0d86c26ccb79c39c8c34b12feb68571d9a
Thomas F. K. Jorna <hello@tefkah.com>
2021-12-11 13:21:15 +0100

    fix: set jest timeout higher

--- src/utils/getListOfCommitsWithStats.spec.ts
+++ src/utils/getListOfCommitsWithStats.spec.ts
@@ -3,7 +3,7 @@ import { getListOfCommitsWithStats } from './getListOfCommitsWithStats'
 const firstCommit = '8a8d96b1a6ae75dd17f7462c31695823189f6f14'
 const lastCommit = '635c1974031c9ba51e275c308ac38617bd8b5b46'
 describe('getListOfCommitsWithStats', () => {
-  jest.setTimeout(90000)
+  jest.setTimeout(200000)
   it('gets a list', async () => {
     const list = await getListOfCommitsWithStats(firstCommit, lastCommit)
     expect(list).toBeDefined()
