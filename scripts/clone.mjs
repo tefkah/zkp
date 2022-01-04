73e05f914418784a499c566767f738b9b0764718
Thomas F. K. Jorna <hello@tefkah.com>
2022-01-04 10:33:42 +0100

    feat(cd): much cleaner setup scripts

--- scripts/clone.mjs
+++ scripts/clone.mjs
@@ -2,13 +2,16 @@ import { clone } from 'isomorphic-git'
 import fs from 'fs'
 import * as http from 'isomorphic-git/http/node/index.cjs'
 import { join } from 'path'
+import { getListOfCommitsWithStats } from 'src/utils/getListOfCommitsWithStats'
 
 const cwd = process.cwd()
+const noteDir = join(cwd, 'notes')
+const noteGitDir = join(cwd, 'notes', 'git')
 await clone({
   fs,
   http,
   url: 'https://github.com/thomasfkjorna/thesis-writing',
-  dir: join(cwd, 'notes'),
-  gitdir: join(cwd, 'notes', 'git'),
+  dir: noteDir,
+  gitdir: noteGitDir,
   remote: 'notes',
 })
