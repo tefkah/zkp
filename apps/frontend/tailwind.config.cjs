1868bf0608d189bdbb56faafa8451f8ed1092a61
Thomas F. K. Jorna <hello@tefkah.com>
2022-09-24 14:10:29 +0200

    instantiate yet another version of the frontend

---
+++ apps/frontend/tailwind.config.cjs
@@ -0,0 +1,16 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind')
+
+const { join } = require('path')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, '**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
